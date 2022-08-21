0569b628e36bd8665092c6d9e8009664f21f1b2f
Teshan Liyanage <teshanuka@gmail.com>
2022-08-21 14:16:23 +0200

    Add glew

--- main.cc
+++ main.cc
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,11 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glew init failed!" << std::endl;
+    }
+    std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
